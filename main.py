22e17dad38b2a655a5f30003a75c6bf8cc6040c6
Guilherme Marques <gm.marques012@gmail.com>
2025-11-11 20:00:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ window = pygame.display.set_mode((600, 480))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("Quitting...")
             pygame.quit()
             quit()
             
